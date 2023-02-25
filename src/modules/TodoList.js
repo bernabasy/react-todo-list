a7ef4a7e38d5d14cb202019850ab7fd3e8eb1f61
Bernabasyosef <bernabasjosef@gmail.com>
2023-02-25 12:39:45 -0800

    Reafactore  TodoList.js

--- src/modules/TodoList.js
+++ src/modules/TodoList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Todo from './Todo';
 
-export default function TodoList({ todos, toggletodo }) {
+const TodoList = TodoList => ({ todos, toggletodo }) {
   return todos.map((todo) => (
     <Todo key={todo.id} toggletodo={toggletodo} todo={todo} />
   ));
 }
+export default TodoList;
